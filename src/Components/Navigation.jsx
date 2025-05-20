66b47c15d73ecc6a1fcc04191f5d24d5404c3c5c
Pakhre Roshan <pakhreroshan@gmail.com>
2025-05-20 22:53:36 +0530

    Done some Changes

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -10,7 +10,9 @@ const Navigation = () => {
   return (
     <>
       <Navbar expand="lg" className="bg-body-dark nav" style={{
+        top: 0,
         width: "100%",
+        zIndex: 1050,
         boxShadow: "0 8px 32px 0 rgba(31, 38, 135, 0.16)",
         backdropFilter: "blur(10px)",
         WebkitBackdropFilter: "blur(10px)",
