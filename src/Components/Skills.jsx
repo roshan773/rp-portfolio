5eea06d6e19140251f491f060e0fa21f9b77e754
Pakhre Roshan <pakhreroshan@gmail.com>
2025-05-20 23:19:53 +0530

    Chnages responsive structure of the skill section

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -25,7 +25,7 @@ const Skills = () => {
           <div className='col-12 col-md-3 mb-3 mb-md-0'>
             <h2 className='fw-bold text-uppercase'>Trending Skill</h2>
           </div>
-          <div className='col-12 col-sm-6 col-md-4 col-lg-3 mt-4 d-flex justify-content-center align-items-center h-100'>
+          <div className='col-6 col-sm-6 col-md-4 col-lg-3 mt-4 d-flex justify-content-center align-items-center h-100'>
             <div className='Prompt text-center' id='icon'>
               <RiRobot2Line size={300} className='p-3 img-fluid' />
               <p className='mt-2 fs-5 fw-bold text-dark'>Prompt Engineering</p>
@@ -41,55 +41,55 @@ const Skills = () => {
           </div>
           <div className='col-12 col-md-9'>
             <div className='row'>
-              <div className='col-12 col-sm-6 col-md-4 col-lg-3 mt-4 d-flex justify-content-center align-items-center h-100'>
+              <div className='col-6 col-sm-6 col-md-4 col-lg-3 mt-4 d-flex justify-content-center align-items-center h-100'>
                 <div className='html text-center'>
                   <FaHtml5 size={300} className='p-4 img-fluid' />
                   <p className='mt-2 fs-5 fw-bold text-dark'>Html</p>
                 </div>
               </div>
-              <div className='col-12 col-sm-6 col-md-4 col-lg-3 mt-4 d-flex justify-content-center align-items-center h-100'>
+              <div className='col-6 col-sm-6 col-md-4 col-lg-3 mt-4 d-flex justify-content-center align-items-center h-100'>
                 <div className='css text-center'>
                   <FaCss3Alt size={300} className='p-4 img-fluid' />
                   <p className='mt-2 fs-5 fw-bold text-dark'>Css</p>
                 </div>
               </div>
-              <div className='col-12 col-sm-6 col-md-4 col-lg-3 mt-4 d-flex justify-content-center align-items-center h-100'>
+              <div className='col-6 col-sm-6 col-md-4 col-lg-3 mt-4 d-flex justify-content-center align-items-center h-100'>
                 <div className='sass text-center'>
                   <FaSass size={300} className='p-4 img-fluid' />
                   <p className='mt-2 fs-5 fw-bold text-dark'>Sass</p>
                 </div>
               </div>
-              <div className='col-12 col-sm-6 col-md-4 col-lg-3 mt-4 d-flex justify-content-center align-items-center h-100'>
+              <div className='col-6 col-sm-6 col-md-4 col-lg-3 mt-4 d-flex justify-content-center align-items-center h-100'>
                 <div className='bootstrap text-center'>
                   <FaBootstrap size={300} className='p-4 img-fluid' />
                   <p className='mt-2 fs-5 fw-bold text-dark'>Bootstrap</p>
                 </div>
               </div>
-              <div className='col-12 col-sm-6 col-md-4 col-lg-3 mt-4 d-flex justify-content-center align-items-center h-100'>
+              <div className='col-6 col-sm-6 col-md-4 col-lg-3 mt-4 d-flex justify-content-center align-items-center h-100'>
                 <div className='jquery text-center'>
                   <DiJqueryLogo size={300} className='p-4 img-fluid' />
                   <p className='mt-2 fs-5 fw-bold text-dark'>Jquery</p>
                 </div>
               </div>
-              <div className='col-12 col-sm-6 col-md-4 col-lg-3 mt-4 d-flex justify-content-center align-items-center h-100'>
+              <div className='col-6 col-sm-6 col-md-4 col-lg-3 mt-4 d-flex justify-content-center align-items-center h-100'>
                 <div className='js text-center'>
                   <IoLogoJavascript size={300} className='p-4 img-fluid' />
                   <p className='mt-2 fs-5 fw-bold text-dark'>Javascript</p>
                 </div>
               </div>
-              <div className='col-12 col-sm-6 col-md-4 col-lg-3 mt-4 d-flex justify-content-center align-items-center h-100'>
+              <div className='col-6 col-sm-6 col-md-4 col-lg-3 mt-4 d-flex justify-content-center align-items-center h-100'>
                 <div className='react text-center'>
                   <FaReact size={300} className='p-4 img-fluid' />
                   <p className='mt-2 fs-5 fw-bold text-dark'>ReactJs</p>
                 </div>
               </div>
-              <div className='col-12 col-sm-6 col-md-4 col-lg-3 mt-4 d-flex justify-content-center align-items-center h-100'>
+              <div className='col-6 col-sm-6 col-md-4 col-lg-3 mt-4 d-flex justify-content-center align-items-center h-100'>
                 <div className='redux text-center'>
                   <SiRedux size={300} className='p-4 img-fluid' />
                   <p className='mt-2 fs-5 fw-bold text-dark'>Redux</p>
                 </div>
               </div>
-              <div className='col-12 col-sm-6 col-md-4 col-lg-3 mt-4 d-flex justify-content-center align-items-center h-100'>
+              <div className='col-6 col-sm-6 col-md-4 col-lg-3 mt-4 d-flex justify-content-center align-items-center h-100'>
                 <div className='Nextjs text-center' id='icon'>
                   <TbBrandNextjs size={300} className='p-4 img-fluid' />
                   <p className='mt-2 fs-5 fw-bold text-dark'>NextJs</p>
@@ -107,7 +107,7 @@ const Skills = () => {
           </div>
           <div className='col-12 col-md-9'>
             <div className='row'>
-              <div className='col-12 col-sm-6 col-md-4 col-lg-3 mt-4 d-flex justify-content-center align-items-center h-100'>
+              <div className='col-6 col-sm-6 col-md-4 col-lg-3 mt-4 d-flex justify-content-center align-items-center h-100'>
                 <div className='firebase text-center'>
                   <IoLogoFirebase size={300} className='p-4 img-fluid' />
                   <p className='mt-2 fs-5 fw-bold text-dark'>FIREBASE</p>
@@ -125,13 +125,13 @@ const Skills = () => {
           </div>
           <div className='col-12 col-md-9'>
             <div className='row'>
-              <div className='col-12 col-sm-6 col-md-4 col-lg-3 mt-4 d-flex justify-content-center align-items-center h-100'>
+              <div className='col-6 col-sm-6 col-md-4 col-lg-3 mt-4 d-flex justify-content-center align-items-center h-100'>
                 <div className='canva text-center'>
                   <SiCanva size={300} className='p-4 img-fluid' />
                   <p className='mt-2 fs-5 fw-bold text-dark'>Canva</p>
                 </div>
               </div>
-              <div className='col-12 col-sm-6 col-md-4 col-lg-3 mt-4 d-flex justify-content-center align-items-center h-100'>
+              <div className='col-6 col-sm-6 col-md-4 col-lg-3 mt-4 d-flex justify-content-center align-items-center h-100'>
                 <div className='github text-center'>
                   <FaGithub size={300} className='p-4 img-fluid' />
                   <p className='mt-2 fs-5 fw-bold text-dark'>Github</p>
