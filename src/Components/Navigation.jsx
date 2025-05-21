81f5c0399a7d7ba412b4ac7a296c37e2240ec334
Pakhre Roshan <pakhreroshan@gmail.com>
2025-05-21 20:56:56 +0530

    Generated navigation sticky

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,34 +1,22 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-// import { IoMoon, IoSunnySharp } from 'react-icons/io5';
-// import { FaBars, FaRegMoon, FaRegSun, FaTimes } from 'react-icons/fa';
-import "./Navigation.css"
+import "./Navigation.css";
 
 const Navigation = () => {
-
   return (
     <>
-      <Navbar expand="lg" className="bg-body-dark nav top-fixed" style={{
-        top: 0,
-        position: "sticky",
-        width: "100%",
+      <Navbar expand="lg" className="bg-body-dark fixed-top nav" style={{
         zIndex: 1050,
         boxShadow: "0 8px 32px 0 rgba(31, 38, 135, 0.16)",
         backdropFilter: "blur(10px)",
         WebkitBackdropFilter: "blur(10px)",
-        border: "1px solid rgba(255, 255, 255, 0.18)",
-        padding: "15px 0",
+        borderBottom: "1px solid rgba(255, 255, 255, 0.18)",
+        padding: "15px 0"
       }}>
         <Container>
-          <Navbar.Brand href="#navigation" id='navigation' className='fw-bold'>RP PORTFOLIO</Navbar.Brand>
-          <Navbar.Toggle
-            aria-controls="navbar"
-            style={{
-              border: "none",
-            }}
-          >
-          </Navbar.Toggle>
+          <Navbar.Brand href="#navigation" id="navigation" className="fw-bold">RP PORTFOLIO</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbar" style={{ border: "none" }} />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="w-100 d-flex justify-content-end">
               <Nav.Link className='fw-bold nav-links' href="#home">HOME</Nav.Link>
@@ -41,7 +29,7 @@ const Navigation = () => {
         </Container>
       </Navbar>
     </>
-  )
+  );
 }
 
-export default Navigation
+export default Navigation;
