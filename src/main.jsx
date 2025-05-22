e2bdf59334df1aab28eff02cea59c300f0bd4896
Pakhre Roshan <pakhreroshan@gmail.com>
2025-05-22 22:37:26 +0530

    remove context api for theme

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { ThemeProvider } from './Context/ThemeContext.jsx'
 // import { BrowserRouter } from 'react-router-dom'
 // import { ThemeProvider } from './Context/ThemeContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <App />
   </>,
 )
